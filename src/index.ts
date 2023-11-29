cee6dfe367e33a15541f8d18d2862f2331ba4ba4
Barry Pitfield <barry@x-ware.co.uk>
2023-11-29 23:53:56 +0000

    export all type defs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import {V1_0_0} from './schemas/locale-schema.js';
-import {OrtacLocaleObjectV1_0_0} from './schemas/locale-d.js';
 import {typeGuards} from './schemas/type-guards.js';
 import {add} from './add.js';
 import {subtract} from './subtract.js';
 
 const schemas = {locale: {V1_0_0}};
 
+export * from './schemas/locale-d.js';
+
 export {
-  OrtacLocaleObjectV1_0_0,
   typeGuards,
   schemas,
   add,
